a5886851d495cbd4d63c8ebcd00a922299bc9cc9
Josep de Cid <josep.de.cid@gmail.com>
2021-11-05 13:54:20 +0100

    Add processRates script for flatdata action

---
+++ processRates.js
@@ -0,0 +1,22 @@
+import { readJSON, writeJSON } from 'https://deno.land/x/flat@0.0.10/src/json.ts'
+
+const filename = 'rates.json'
+const json = await readJSON(filename)
+
+const date = json.PVPC[0].Dia;
+const day = date[0]
+const month = date[1]
+const year = date[2]
+
+const filteredData = json.PVPC.map(({ Hora, PCB, CYM }) => {
+    return {
+        hour: Hora.split('-')[0],
+        price: {
+            PCB: parseFloat(PCB.replace(',', '.')) / 1000,
+            CYM: parseFloat(CYM.replace(',', '.')) / 1000
+        }
+    }
+})
+
+const newFilename = 'data/processed/' + day + '_' + month + '_' + year + '.json'
+await writeJSON(newFilename, filteredData)
