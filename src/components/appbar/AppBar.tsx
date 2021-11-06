e18f72493aa3ed609907cc13399cb38c92ec00e5
Josep de Cid <josep.de.cid@gmail.com>
2021-11-06 02:15:15 +0100

    Uncomment logo

--- src/components/appbar/AppBar.tsx
+++ src/components/appbar/AppBar.tsx
@@ -7,10 +7,10 @@ export default function AppBar() {
     return (
         <nav className="fixed z-10 flex items-center justify-between w-full p-3 bg-teal-500">
             <div className="flex items-center flex-shrink-0 mr-6 text-white">
-                {/*<img
-                    src="/assets/favicon.png" alt={`${t('APP_NAME')} logo`}
+                <img
+                    src="/favicon.ico" alt={`${t('APP_NAME')} logo`}
                     className="w-10 h-10 mr-1"
-                />*/}
+                />
                 <span className="text-2xl font-bold">
                     {t('APP_NAME')}
                 </span>
